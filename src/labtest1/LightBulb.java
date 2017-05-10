0aa5c9f02b474a1fcc1ec96fca19856b28b0386b
kanishka <kanishkabhandari10@gmail.com>
2017-05-10 11:51:30 -0700

    commit message3
    Signed-off-by: kanishka <kanishkabhandari10@gmail.com>

--- src/labtest1/LightBulb.java
+++ src/labtest1/LightBulb.java
@@ -5,8 +5,8 @@ public class LightBulb {
 	private int bulbwattage;
 	
 
-	public LightBulb(int watt){
-		this.bulbwattage=watt;
+	public LightBulb(int wat){
+		this.bulbwattage=wat;
 	}
 	public int getBulbWattage(){
 		return this.bulbwattage;
